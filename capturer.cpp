5d4802ab9755393dfa1fe2287ef76b6016475f14
Angelos Anto <aggelos.anto@gmail.com>
2020-06-13 19:22:22 +0300

    Support for both opencv2.4 and opencv3.4

--- capturer.cpp
+++ capturer.cpp
@@ -16,7 +16,7 @@
 #include <dirent.h>
 
 //OpenCV includes
-#include <opencv2/contrib/contrib.hpp>
+//#include <opencv2/contrib/contrib.hpp>
 #include <opencv2/core/core.hpp>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
